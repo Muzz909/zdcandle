85cc2c01aa71ff76f5adddef1813bc78a9971b82
Muzz909 <muzaina.tauqeer2021@gmail.com>
2026-04-17 02:22:36 +0530

    Update kite_auth.py

--- kite_auth.py
+++ kite_auth.py
@@ -1,13 +1,14 @@
 from kiteconnect import KiteConnect
+from dotenv import load_dotenv
+import os
 
-API_KEY = "kx2db0cpkatmg6iz"
-API_SECRET = "v7ere5zlfqqgqafuxroyeeq06ioaucp9"
+load_dotenv()
+
+API_KEY = os.getenv("kx2db0cpkatmg6iz")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
 kite = KiteConnect(api_key=API_KEY)
+kite.set_access_token(ACCESS_TOKEN)
 
-def get_login_url():
-    return kite.login_url()
-
-def generate_access_token(request_token):
-    data = kite.generate_session(request_token, api_secret=API_SECRET)
-    return data["access_token"]
+def get_kite():
+    return kite
